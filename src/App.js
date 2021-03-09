77b5bc3794b3844701f4b6e3d158c1c39544d673
Shiva Kanyal <kanyalshiva1@gmail.com>
2021-03-09 11:39:54 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "font-awesome/css/font-awesome.css";
+import Movies from "./components/movies";
+function App() {
+  return (
+    <main className="container">
+      <Movies />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/movies.jsx
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import { getMovies } from "../services/fakeMovieService";
+
+class Movies extends Component {
+  state = {
+    movies: getMovies(),
+  };
+  handleDelete(id) {
+    let movies = this.state.movies.filter((movie) => movie._id !== id);
+    this.setState({ movies });
+  }
+  render() {
+    if (this.state.movies.length === 0) {
+      return <p>No movies in database</p>;
+    }
+    return (
+      <div>
+        <p>Showing {this.state.movies.length} movies in database</p>
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">Title</th>
+              <th scope="col">Genre</th>
+              <th scope="col">Stock</th>
+              <th scope="col">Rate</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.map((movie) => (
+              <tr>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <button
+                    className="btn btn-danger btn-sm"
+                    onClick={() => {
+                      this.handleDelete(movie._id);
+                    }}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default Movies;
