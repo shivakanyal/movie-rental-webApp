ff2996def0cb5f0055b4531233ff4278ce2e4b51
Shiva Kanyal <kanyalshiva1@gmail.com>
2021-03-10 15:00:28 +0530

    type checking is done for pagination component using prop-types package

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import _ from "lodash";
-
+import PropTypes from "prop-types";
 const Pagination = (props) => {
   const { itemsCount, pageSize, onPageChange, currentPage } = props;
   const pagesCount = Math.ceil(itemsCount / pageSize);
@@ -22,6 +22,12 @@ const Pagination = (props) => {
       </ul>
     </nav>
   );
+};
+Pagination.propTypes = {
+  itemsCount: PropTypes.number.isRequired,
+  pageSize: PropTypes.number.isRequired,
+  currentPage: PropTypes.number.isRequired,
+  onPageChange: PropTypes.func.isRequired,
 };
 
 export default Pagination;
