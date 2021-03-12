e8d8f72b18389678b7e3812756c74cde8ec272c3
Shiva Kanyal <kanyalshiva1@gmail.com>
2021-03-12 21:17:46 +0530

    navBar is added

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
 import Movies from "./components/movies";
+import NavBar from "./components/navbar";
 function App() {
   return (
     <main className="container">
+      <NavBar />
       <Movies />
     </main>
   );
